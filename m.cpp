ae4b7a1bf6e8becb0d967424ced523079449d937
Your Name <peshkovct@gmail.com>
2022-12-07 13:47:11 +0300

    ererere

--- m.cpp
+++ m.cpp
@@ -1,13 +1,51 @@
-//#include <iostream>
-#include "mass2d.h"
+#include <iostream>
+#include <iomanip>
+//#include "mass2d.h"
 
-using namespace N;
+using namespace std;
 
 int main(){
-    int x[]={1,2,3,4,5,6,7};
-    int size=7;
-    run(x,size);
-    
+    int sz = 5;
+	int** m = new int*[sz];
+	double r=0.0;
+	for (int i = 0;i < sz;i++) {
+		m[i] = new int[sz];
+		for (int j = 0;j < sz;j++){
+			 m[i][j] = 3 * i +j;
+            r+=m[i][j];
+        }
+	}
+
+    r=r/(sz*sz);
+
+
+
+    cout<<"avg "<<r<<endl;
+    int f=1;
+    int first;
+    for (int i = 0;i < sz;i++) {
+		for (int j = 0;j < sz;j++)
+			if (m[i][j]>r  && f==1){
+                first=m[i][j];
+                f=0;
+            }
+            else {
+                if (m[i][j]<first && first>r)
+                    first=m[i][j];
+            }
+		cout << endl;
+	}
+    cout<<"elem "<<first<<endl;
+    for (int i = 0;i < sz;i++) {
+		for (int j = 0;j < sz;j++)
+			cout<<setw(5)<<m[i][j];
+		cout << endl;
+	}
+
+	for (int i = 0;i < sz;i++) {
+		delete[] m[i];
+	}
+	delete[] m;
 
 
 }
--- mass2d.h
+++ mass2d.h
@@ -10,6 +10,34 @@ void run(int mass[], int size){
     }
     cout<<endl;
 }
+
+void swap(int list[], int i, int j)
+{
+    int temp=list[i];
+    list[i]=list[j];
+    list[j]=temp;
+}
     
+
+int part(int list[], int first, int last)
+{
+
+    int p;
+    swap(list,first,(first+last)/2);
+    p=list[first];
+    int small=first;
+    
+    for (int index=first+1;index<=last;++index){
+            if (list[index]<p){
+                small++;
+                swap(list,small,index);
+            }
+    }
+    swap(list,first,small);
+    return small;
+
+}
+
+
 
 }
